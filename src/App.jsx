b6d964be411d69aef69ad747287603eb023250ea
JsH <jsh1147@naver.com>
2023-09-30 02:04:13 +0900

    feat: 라우터 환경 구성
    v6.4 이상부터 지원하는 RouterProvider로 구성함
    (기존 기능과 더불어 DATA API를 지원함)
    
    Related #1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Outlet />}>
+      <Route path="" element={<h1>Main</h1>} />
+    </Route>
+  )
+);
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
