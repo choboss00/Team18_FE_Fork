081b32505fc4c67e201564ae1c9b303071295b87
JsH <jsh1147@naver.com>
2023-09-30 06:25:25 +0900

    style: App 파일 포맷팅
    App.jsx 내 불필요 코드를 제거함
    index.css와 중복되는 App.css를 제거함

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Outlet,
   Route,
   Navigate,
   RouterProvider,
@@ -60,8 +55,6 @@ const router = createBrowserRouter(
 );
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <RouterProvider router={router} />;
 }
 
