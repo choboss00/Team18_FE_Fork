a36a044ec8ea19f68a32252d6d7f6a93eaba22d4
JsH <jsh1147@naver.com>
2023-09-30 05:17:48 +0900

    feat: 페이지 라우팅 구현
    와이어프레임의 페이지들을 참고해 라우팅을 구현함
    페이지 구조에 맞춰 레이아웃과 Auth 확인을 적용함
    부적절한 URL이 요청될 경우 NotFound로 연결함
    
    Related #1

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,66 @@ import {
   createRoutesFromElements,
   Outlet,
   Route,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Outlet />}>
-      <Route path="" element={<h1>Main</h1>} />
+    <Route>
+      <Route index element={<Navigate to="watching/videos" />} />
+      <Route path="login" element={<h1>LogIn</h1>} />
+      <Route path="signup" element={<h1>SignUp</h1>} />
+      <Route
+        element={
+          <>
+            <h1>AuthCheck</h1>
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="interest" element={<h1>Interest</h1>} />
+      </Route>
+      <Route
+        element={
+          <>
+            <h1>Layout</h1>
+            <Outlet></Outlet>
+          </>
+        }
+      >
+        <Route path="watching/videos" element={<h1>Vidoes</h1>} />
+        <Route path="watching/video/:videoId" element={<h1>Video</h1>} />
+        <Route path="mentoring/posts" element={<h1>Posts</h1>} />
+        <Route path="mentoring/post/:postId" element={<h1>Post</h1>} />
+        <Route path="chatting/rooms" element={<h1>Rooms</h1>} />
+        <Route
+          path="chatting/roomprofile/:roomId"
+          element={<h1>RoomProfile</h1>}
+        />
+        <Route
+          element={
+            <>
+              <h1>AuthCheck</h1>
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="watching/history" element={<h1>History</h1>} />
+          <Route path="mentoring/write" element={<h1>PostWrite</h1>} />
+          <Route path="mentoring/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="chatting/room/:roomId" element={<h1>Room</h1>} />
+          <Route path="chatting/create" element={<h1>RoomCreate</h1>} />
+          <Route path="mypage/profile" element={<h1>Profile</h1>} />
+          <Route path="mypage/profile/fix" element={<h1>ProfileFix</h1>} />
+          <Route path="mypage/information" element={<h1>Information</h1>} />
+          <Route
+            path="mypage/information/fix"
+            element={<h1>InformationFix</h1>}
+          />
+        </Route>
+      </Route>
+      <Route path="*" element={<h1>NotFound</h1>} />
     </Route>
   )
 );
