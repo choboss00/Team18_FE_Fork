6bbec13faad350450dc9cefac6f2a4160540249c
JsH <jsh1147@naver.com>
2023-09-30 05:48:46 +0900

    feat: 라우팅 임시 페이지 생성
    멘토링 관련 페이지에 한하여 임시 페이지를 생성함
    생성한 페이지를 라우팅 로직에 연결함
    
    Related #1

--- src/App.jsx
+++ src/App.jsx
@@ -11,50 +11,38 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import AuthCheck from "./layouts/AuthCheck.jsx";
+import Layout from "./layouts/Layout.jsx";
+import Posts from "./pages/mentoring/Posts.jsx";
+import Post from "./pages/mentoring/Post.jsx";
+import Write from "./pages/mentoring/Write.jsx";
+import Edit from "./pages/mentoring/Edit.jsx";
+import Dashboard from "./pages/mentoring/Dashboard.jsx";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route index element={<Navigate to="watching/videos" />} />
       <Route path="login" element={<h1>LogIn</h1>} />
       <Route path="signup" element={<h1>SignUp</h1>} />
-      <Route
-        element={
-          <>
-            <h1>AuthCheck</h1>
-            <Outlet />
-          </>
-        }
-      >
+      <Route element={<AuthCheck />}>
         <Route path="interest" element={<h1>Interest</h1>} />
       </Route>
-      <Route
-        element={
-          <>
-            <h1>Layout</h1>
-            <Outlet></Outlet>
-          </>
-        }
-      >
+      <Route element={<Layout />}>
         <Route path="watching/videos" element={<h1>Vidoes</h1>} />
         <Route path="watching/video/:videoId" element={<h1>Video</h1>} />
-        <Route path="mentoring/posts" element={<h1>Posts</h1>} />
-        <Route path="mentoring/post/:postId" element={<h1>Post</h1>} />
+        <Route path="mentoring/posts" element={<Posts />} />
+        <Route path="mentoring/post/:postId" element={<Post />} />
         <Route path="chatting/rooms" element={<h1>Rooms</h1>} />
         <Route
           path="chatting/roomprofile/:roomId"
           element={<h1>RoomProfile</h1>}
         />
-        <Route
-          element={
-            <>
-              <h1>AuthCheck</h1>
-              <Outlet />
-            </>
-          }
-        >
+        <Route element={<AuthCheck />}>
           <Route path="watching/history" element={<h1>History</h1>} />
-          <Route path="mentoring/write" element={<h1>PostWrite</h1>} />
-          <Route path="mentoring/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="mentoring/write" element={<Write />} />
+          <Route path="mentoring/edit/:postId" element={<Edit />} />
+          <Route path="mentoring/dashboard" element={<Dashboard />} />
           <Route path="chatting/room/:roomId" element={<h1>Room</h1>} />
           <Route path="chatting/create" element={<h1>RoomCreate</h1>} />
           <Route path="mypage/profile" element={<h1>Profile</h1>} />
