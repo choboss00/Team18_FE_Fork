6fbcaa7b968e4a51dbad3e76fe627c6764b0d594
chaemina <chaemina82@naver.com>
2023-10-08 04:04:44 +0900

    feat : 회원가입 컴포넌트 및 폼 구현
    Login 폼과 동일하게 입력란은 inputBox에 props를 전달하도록 구현함
    국가 선택의 경우, COUNTRY.js 상수 컴포넌트를 생성하여 국가 이름 배열을
    저장하고 dropdown option의 prop으로 전달함
    *멘토 선택 미 구현

--- src/components/account/atoms/InputBox.jsx
+++ src/components/account/atoms/InputBox.jsx
@@ -1,14 +1,14 @@
-const InputBox = (props) => {
+const InputBox = ({ className, ...props }) => {
   return (
     <>
-      <label>
+      <label htmlFor={props.id} className={`${className}`}>
         {props.label}
         <input
           id={props.id}
           type={props.type}
           value={props.value}
           placeholder={props.placeholder}
-          className="mt-4 mb-4 p-4 w-[100%] text-base"
+          className="border-b-2 border-orange focus:border-b-0 focus:outline-none focus:ring focus:ring-orange  mt-4 mb-4 p-4 w-[100%] text-base"
         ></input>
       </label>
     </>
--- src/components/account/atoms/Title.jsx
+++ src/components/account/atoms/Title.jsx
@@ -1,5 +1,9 @@
-const Title = ({ children }) => {
-  return <h3 className="text-4xl font-bold text-green-700">{children}</h3>;
+const Title = ({ children, className }) => {
+  return (
+    <h3 className={`text-4xl font-bold text-green-700 ${className}`}>
+      {children}
+    </h3>
+  );
 };
 
 export default Title;
