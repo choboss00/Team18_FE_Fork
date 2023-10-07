5b1b8905e958196f54221c120ebc9f508b9c6e76
chaemina <chaemina82@naver.com>
2023-10-07 22:59:14 +0900

    refactoring : Link component로 수정
    a 태그는 클릭 시 새로운 페이지를 불러오기 때문에  Link component로
    수정함

--- src/components/account/templates/Logintemplate.jsx
+++ src/components/account/templates/Logintemplate.jsx
@@ -2,6 +2,7 @@ import Title from "../atoms/Title";
 import Footer from "../../../layouts/Footer";
 import LoginForm from "../organisms/LoginForm";
 import LOGIN from "../constants/LOGIN";
+import { Link } from "react-router-dom";
 
 const LoginTemplate = () => {
   return (
@@ -12,9 +13,9 @@ const LoginTemplate = () => {
         <div className="mt-4">
           <p className="text-sm text-blue-900 ml-2">
             No account yet?{" "}
-            <a href="/signup" className="text-sm font-bold ml-2 text-blue-900">
+            <Link to="/signup" className="text-sm font-bold ml-2 text-blue-900">
               Sign Up
-            </a>
+            </Link>
           </p>
         </div>
       </main>
