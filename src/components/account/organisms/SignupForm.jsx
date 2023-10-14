c99d07004b36fee394d901a8ad76823f2e754005
chaemina <chaemina82@naver.com>
2023-10-15 04:27:52 +0900

    feat : Role check radio button 구현
    checkbox 컴포넌트를 생성하여, 회원가입 폼에서 사용함
    type을 radio로 지정하여 하나의 값만 선택 가능하게 함

--- src/components/account/organisms/SignupForm.jsx
+++ src/components/account/organisms/SignupForm.jsx
@@ -2,80 +2,126 @@ import InputBox from "../atoms/InputBox";
 import Button from "../../common/Button";
 import Dropdown from "../../common/Dropdown";
 import { useState } from "react";
-import COUNTRIES from "../constants/COUNTRY";
-import useValidation from "../hooks/useValidation";
+import { useForm, FormProvider, Controller } from "react-hook-form";
+import COUNTRY from "../constants/COUNTRY";
+import CheckBox from "../atoms/CheckBox";
 
 const SignupForm = ({ inputProps }) => {
+  const methods = useForm();
+
   const [selectedOption, setSelectedOption] = useState("the United States");
 
   const handleOptionChange = (selectedOption) => {
     setSelectedOption(selectedOption);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const [selectedRole, setSelectedRole] = useState("");
+
+  const handleRoleChange = (event) => {
+    setSelectedRole(event.target.value);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data, selectedRole);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="w-[500px]">
-          <div className="flex gap-10">
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <div className="w-[500px]">
+            <div className="flex gap-10">
+              {inputProps
+                .filter(
+                  (props) =>
+                    props.name === "FirstName" || props.name === "LastName"
+                )
+                .map((inputField) => {
+                  return (
+                    <Controller
+                      name={inputField.name}
+                      key={inputField.name}
+                      control={methods.control}
+                      defaultValue=""
+                      rules={inputField.rules}
+                      render={({ field, fieldState }) => (
+                        <InputBox
+                          {...field}
+                          id={inputField.name}
+                          label={inputField.label}
+                          variant={inputField.variant}
+                          type={inputField.type}
+                          placeholder={inputField.placeholder}
+                          error={fieldState.invalid}
+                          helperText={
+                            fieldState.invalid ? fieldState.error.message : ""
+                          }
+                          triggerValidation={methods.trigger}
+                        />
+                      )}
+                    />
+                  );
+                })}
+            </div>
             {inputProps
               .filter(
-                (props) => props.id === "FirstName" || props.id === "LastName"
+                (inputField) =>
+                  inputField.name !== "FirstName" &&
+                  inputField.name !== "LastName"
               )
               .map((inputField) => {
-                const validationHook = useValidation("", inputField.error);
                 return (
-                  <InputBox
-                    key={inputField.id}
-                    id={inputField.id}
-                    label={inputField.label}
-                    type={inputField.type}
-                    placeholder={inputField.placeholder}
-                    variant={inputField.variant}
-                    error={validationHook.error}
-                    msg={inputField.msg}
-                    value={validationHook.value}
-                    onChange={validationHook.handleChange}
+                  <Controller
+                    name={inputField.name}
+                    key={inputField.name}
+                    control={methods.control}
+                    defaultValue=""
+                    rules={inputField.rules}
+                    render={({ field, fieldState }) => (
+                      <InputBox
+                        {...field}
+                        id={inputField.name}
+                        label={inputField.label}
+                        variant={inputField.variant}
+                        type={inputField.type}
+                        placeholder={inputField.placeholder}
+                        error={fieldState.invalid}
+                        helperText={
+                          fieldState.invalid ? fieldState.error.message : ""
+                        }
+                        triggerValidation={methods.trigger}
+                      />
+                    )}
                   />
                 );
               })}
+            <CheckBox
+              value="mentor"
+              type="radio"
+              checked={selectedRole === "mentor"}
+              onChange={handleRoleChange}
+            >
+              Mentor
+            </CheckBox>
+            <CheckBox
+              value="mentee"
+              type="radio"
+              checked={selectedRole === "mentee"}
+              onChange={handleRoleChange}
+            >
+              Mentee
+            </CheckBox>
+            <Dropdown
+              options={COUNTRY.map((country) => country.name)}
+              selected={selectedOption}
+              onSelectedChange={handleOptionChange}
+            />
+            <Button color="orange" size="xl">
+              Sign Up
+            </Button>
           </div>
-          {inputProps
-            .filter(
-              (inputField) =>
-                inputField.id !== "FirstName" && inputField.id !== "LastName"
-            )
-            .map((inputField) => {
-              const validationHook = useValidation("", inputField.error);
-              return (
-                <InputBox
-                  key={inputField.id}
-                  id={inputField.id}
-                  label={inputField.label}
-                  type={inputField.type}
-                  placeholder={inputField.placeholder}
-                  variant={inputField.variant}
-                  error={validationHook.error}
-                  msg={inputField.msg}
-                  value={validationHook.value}
-                  onChange={validationHook.handleChange}
-                />
-              );
-            })}
-          <Dropdown
-            options={COUNTRIES.map((index) => index.name)}
-            selected={selectedOption}
-            onSelectedChange={handleOptionChange}
-          />
-
-          <Button color="orange" size="xl">
-            Sign Up
-          </Button>
-        </div>
-      </form>
+        </form>
+      </FormProvider>
     </>
   );
 };
