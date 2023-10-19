258c9a8043006f524aed44f11e4b01bb22d27d32
JsH <jsh1147@naver.com>
2023-10-19 20:29:53 +0900

    fix: useEffectOnce 명칭 통일
    useEffectOnce의 파일명과 함수명이 달라서 통일함

--- src/layouts/AuthCheck.jsx
+++ src/layouts/AuthCheck.jsx
@@ -1,10 +1,10 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import { useAuthCheck } from "../hooks/useEffectOnce";
+import { useEffectOnce } from "../hooks/useEffectOnce";
 
 export default function AuthCheck() {
   const navigate = useNavigate();
 
-  useAuthCheck(() => {
+  useEffectOnce(() => {
     if (!window.localStorage.getItem("token"))
       if (window.confirm("로그인이 필요한 서비스입니다.\n로그인 하시겠습니까?"))
         navigate("/users/login", { replace: true });
