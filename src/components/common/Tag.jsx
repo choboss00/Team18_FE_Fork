ebe131382dce028c240cb0d2d8c5d7111f843861
chaemina <chaemina82@naver.com>
2023-10-23 23:23:11 +0900

    refactoring : tag css 수정
    태그의 border 부분의 rounded 값을 더 주어 부자연스러워 보이지 않도록 함
    국기의 경우 border 값을 지워 답답해보이지 않도록 함
    멘토/멘티 태그는 다른 태그들과 구분 되도록 background 대신 border
    과 text color를 주고 이는 프로젝트의 포인트 컬러로 지정해줌

--- src/components/common/Tag.jsx
+++ src/components/common/Tag.jsx
@@ -1,22 +1,22 @@
 const tagType = {
   Sport: "bg-blue-500 text-white",
-  Game: "bg-teal-500 text-white",
+  Game: "bg-sky-500 text-white",
   BTS: "bg-red-500 text-white",
   Drama: "bg-yellow-500 text-white",
   Soccer: "bg-purple-500 text-white",
   "K-pop": "bg-pink-500 text-white",
   Baseball: "bg-rose-500 text-white",
-  LOL: "bg-lime-500 text-white",
-  Youtuber: "bg-emerald-500 text-white",
-  Mentor: "bg-blue-400 text-black",
-  Mentee: "bg-yellow-100 text-black",
+  LOL: "bg-indigo-500 text-white",
+  Youtuber: "bg-fuchsia-500 text-white",
+  Mentor: "border-pink-400 text-pink-400 font-bold",
+  Mentee: "border-green-500 text-green-500 font-bold",
 };
 
 const Tag = ({ children, ...props }) => {
   const tagStyle = `${
     tagType[children]
-  } py-1 px-2 text-xs rounded-lg border-2 ${
-    children.includes("Ment") ? "border-black" : "border-[#0000]"
+  } py-1 px-2 text-xs rounded-xl border-2 ${
+    children.includes("Ment") ? "" : "border-[#0000]"
   }`;
 
   return (
