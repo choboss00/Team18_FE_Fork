04276dad0df63feac987535c4d2dfa39f1c92234
chaemina <chaemina82@naver.com>
2023-10-30 01:55:08 +0900

    feat : Information page 생성
    사용자 정보를 불러오는 Information Page를 생성함
    공통 컴포넌트 Loader와 Error 사용 전에 임시로 atom 폴더에 생성함
    Error 컴포넌트는 에러 발생 시 페이지를 리로드 할 수 있도록 버튼을 추가함

---
+++ src/components/account/atoms/Error.jsx
@@ -0,0 +1,22 @@
+import Button from "../../common/Button";
+
+const Error = ({ errorMessage = "" }) => {
+  return (
+    <main className="mt-20 justify-center items-center flex flex-col">
+      <div className="p-10 max-w-[500px] text-center">
+        <p className="text-4xl font-bold text-red-500">
+          Please try again later !
+        </p>
+        <p className="mt-10 mb-10 font-bold text-gray-400">{errorMessage}</p>
+        <Button
+          color="white"
+          size="xl"
+          onClick={() => window.location.reload()}
+        >
+          Page Reload
+        </Button>
+      </div>
+    </main>
+  );
+};
+export default Error;
