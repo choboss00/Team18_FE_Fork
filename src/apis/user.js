d3f9911015033a5862d7466ecc03f3195d3bdcea
chaemina <chaemina82@naver.com>
2023-11-06 16:33:34 +0900

    fix : backend api 연결
    mock api 에서 실제 서버와 통신하는 api 코드로 수정함

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,5 +1,5 @@
 import { instance } from "./instance";
-import { mockUsers, mockResponse } from "../components/account/mockUser";
+// import { mockUsers, mockResponse } from "../components/account/mockUser";
 
 // export const emailCheck = (data) => {
 //   const email = data;
@@ -36,41 +36,63 @@ export const register = (data) => {
   });
 };
 
-// export const login = (data) => {
-//   const { email, password } = data;
-//   return instance.post("/users/login", {
-//     email,
-//     password,
-//   });
-// };
-
-// 백앤드 api 연결 전 까지 mock api 사용 - login
-export const login = async (data) => {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-
-  // 사용자의 email이 mock data의 key 값과 일치하면
-  const user = mockUsers[data.email];
-  // 패스워드 일치 확인
-  if (user && user.password === data.password) {
-    const responseUser = {
-      uid: user.uid,
-    };
-    return mockResponse({
-      message: "Login successful",
-      user: responseUser,
-      success: true,
-    });
-  } else {
-    return mockResponse({
-      message: "Invalid email or password",
-      success: false,
-    });
-  }
+export const login = (data) => {
+  const { email, password } = data;
+  return instance.post("/users/login", {
+    email,
+    password,
+  });
 };
 
 // export const getUser = () => {
 //   return instance.get("/getUser");
 // };
+
+// ---- mock api
+
+// 백앤드 api 연결 전 까지 mock api 사용 - login
+// export const login = async (data) => {
+//   await new Promise((resolve) => setTimeout(resolve, 500));
+
+//   // 사용자의 email이 mock data의 key 값과 일치하면
+//   const user = mockUsers[data.email];
+//   // 패스워드 일치 확인
+//   if (user && user.password === data.password) {
+//     const responseUser = {
+//       uid: user.uid,
+//     };
+//     return mockResponse({
+//       message: "Login successful",
+//       user: responseUser,
+//       success: true,
+//     });
+//   } else {
+//     return mockResponse({
+//       message: "Invalid email or password",
+//       success: false,
+//     });
+//   }
+// };
+
+// 백앤드 api 연결 전 까지 mock api 사용 - emailCheck
+export const emailCheck = async (data) => {
+  const { email } = data;
+  let response;
+
+  // mockUsers 객체에서 이메일 찾기
+  if (mockUsers[email]) {
+    // 이메일이 존재하면 success: false 리턴
+    response = mockResponse({
+      success: false,
+      message: "Email already exists.",
+    });
+  } else {
+    // 이메일이 존재하지 않으면 success: true 리턴
+    response = mockResponse({ success: true, message: "Email is available." });
+  }
+
+  return response;
+};
 
 // 백앤드 api 연결 전 까지 mock api 사용 - getUser
 // 임시 uid로 사용자 판별
@@ -102,24 +124,4 @@ export const getUser = async () => {
     user: responseUser,
     success: true,
   };
-};
-
-// 백앤드 api 연결 전 까지 mock api 사용 - emailCheck
-export const emailCheck = async (data) => {
-  const { email } = data;
-  let response;
-
-  // mockUsers 객체에서 이메일 찾기
-  if (mockUsers[email]) {
-    // 이메일이 존재하면 success: false 리턴
-    response = mockResponse({
-      success: false,
-      message: "Email already exists.",
-    });
-  } else {
-    // 이메일이 존재하지 않으면 success: true 리턴
-    response = mockResponse({ success: true, message: "Email is available." });
-  }
-
-  return response;
 };
