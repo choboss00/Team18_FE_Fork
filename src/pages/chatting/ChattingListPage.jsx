415e9b9d98e3aff215e68bf23a76f1bf70e60275
bada308 <203190@jnu.ac.kr>
2023-11-10 20:29:42 +0900

    feat: useTalkLogin 훅 적용 - 채팅 리스트 페이지 - 채팅방 페이지
    Related #54

--- src/pages/chatting/ChattingListPage.jsx
+++ src/pages/chatting/ChattingListPage.jsx
@@ -6,19 +6,23 @@ import {
   currentChannelTypeAtom,
   userIdAtom,
 } from "../../store/chatting/chatting";
-import { login } from "../../apis/chatting/talkplus";
+import { client, login } from "../../apis/chatting/talkplus";
 import Tabs from "../../components/chatting/channelList/Tabs";
 import CreateChannelModal from "../../components/chatting/modal/CreateChannelModal";
 import Fallback from "../../components/common/Fallback";
 import Loader from "../../components/common/Loader";
 import ChannelListItemSkeletons from "../../components/chatting/skeleton/ChannelListItemSkeletons";
 import Error from "../../components/common/Error";
+import { simpleUserInfo } from "../../apis/mypage";
+import { useTalkLogin } from "../../hooks/useTalkLogin";
 
 const ChattingListPage = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const channelType = useAtomValue(currentChannelTypeAtom);
   const setUserId = useSetAtom(userIdAtom);
 
+  const { isLogin, userId } = useTalkLogin(client, simpleUserInfo, login);
+  setUserId(userId);
 
   return (
     <Fallback
@@ -26,29 +30,31 @@ const ChattingListPage = () => {
       Error={Error}
       errorMessage="Failed to load chatting list page"
     >
-      <div className="w-[58rem] mx-auto my-16 flex flex-col space-y-5">
-        <CreateChannelModal
-          modalIsOpen={modalIsOpen}
-          setModalIsOpen={setModalIsOpen}
-        />
-        <section className="flex flex-col gap-6 w-[1000px]">
-          <h1 className="inline-block text-4xl font-bold text-green-700">
-            Chat List
-          </h1>
-          <Tabs handleModalOpen={() => setModalIsOpen(true)} />
+      {isLogin && (
+        <div className="w-[58rem] mx-auto my-16 flex flex-col space-y-5">
+          <CreateChannelModal
+            modalIsOpen={modalIsOpen}
+            setModalIsOpen={setModalIsOpen}
+          />
+          <section className="flex flex-col gap-6 w-[1000px]">
+            <h1 className="inline-block text-4xl font-bold text-green-700">
+              Chat List
+            </h1>
+            <Tabs handleModalOpen={() => setModalIsOpen(true)} />
             <Fallback
               Loader={ChannelListItemSkeletons}
               Error={Error}
               errorMessage="Failed to load channel list"
             >
-          {channelType === "Public" ? (
-            <PublicChannelList />
-          ) : (
-            <JoinedChannelList />
-          )}
+              {channelType === "Public" ? (
+                <PublicChannelList />
+              ) : (
+                <JoinedChannelList />
+              )}
             </Fallback>
-        </section>
-      </div>
+          </section>
+        </div>
+      )}
     </Fallback>
   );
 };
--- src/pages/chatting/ChattingRoomsPage.jsx
+++ src/pages/chatting/ChattingRoomsPage.jsx
@@ -9,34 +9,17 @@ import ChannelSetting from "../../components/chatting/channelRoom/channelSetting
 import Fallback from "../../components/common/Fallback";
 import Error from "../../components/common/Error";
 import Loader from "../../components/common/Loader";
+import { simpleUserInfo } from "../../apis/mypage";
+import { useTalkLogin } from "../../hooks/useTalkLogin";
 
 const ChattingRoomsPage = () => {
   const setUserId = useSetAtom(userIdAtom);
-  const setUserProfileImageUrl = useSetAtom(userProfileImageUrlAtom);
-  const setUserName = useSetAtom(userNameAtom);
-  const [isLogin, setIsLogin] = useState(client.isLoggedIn());
   const [isChannelDetailModalOpen, setIsChannelDetailModalOpen] =
     useState(false);
   const channelId = useParams().roomId;
 
-  const handleLogin = () => {
-    login()
-      .then((res) => {
-        setUserId(res.id);
-        setUserProfileImageUrl(res.profileImageUrl);
-        setUserName(res.username);
-        setIsLogin(client.isLoggedIn());
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    handleLogin();
-  }, []);
-
-  if (!isLogin) return <></>;
+  const { isLogin, userId } = useTalkLogin(client, simpleUserInfo, login);
+  setUserId(userId);
 
   return (
     <Fallback
@@ -44,28 +27,30 @@ const ChattingRoomsPage = () => {
       Error={Error}
       errorMessage="메세지를 불러오는데 실패했습니다."
     >
-      <div className="flex w-[95vw] overflow-x-hidden">
-        <div className="flex justify-center items-start gap-7 h-fit mt-5 w-full">
-          <Link to={`/chatting/rooms`}>
-            <span className="material-symbols-outlined text-3xl font-black mt-6">
-              arrow_back
-            </span>
-          </Link>
-          <div className="flex flex-col w-[1000px] max-w-full h-fit my-6 relative">
-            <span
-              className="material-symbols-outlined w-full bg-white font-semibold text-3xl p-2 border-b-2 text-end"
-              onClick={() => setIsChannelDetailModalOpen((prev) => !prev)}
-            >
-              menu
-            </span>
-            {isChannelDetailModalOpen && (
-              <ChannelSetting channelId={channelId} />
-            )}
-            <MessageList channelId={channelId} />
-            <MessageInput channelId={channelId} />
+      {isLogin && (
+        <div className="flex w-[95vw] overflow-x-hidden">
+          <div className="flex justify-center items-start gap-7 h-fit mt-5 w-full">
+            <Link to={`/chatting/rooms`}>
+              <span className="material-symbols-outlined text-3xl font-black mt-6">
+                arrow_back
+              </span>
+            </Link>
+            <div className="flex flex-col w-[1000px] max-w-full h-fit my-6 relative">
+              <span
+                className="material-symbols-outlined w-full bg-white font-semibold text-3xl p-2 border-b-2 text-end"
+                onClick={() => setIsChannelDetailModalOpen((prev) => !prev)}
+              >
+                menu
+              </span>
+              {isChannelDetailModalOpen && (
+                <ChannelSetting channelId={channelId} />
+              )}
+              <MessageList channelId={channelId} />
+              <MessageInput channelId={channelId} />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </Fallback>
   );
 };
