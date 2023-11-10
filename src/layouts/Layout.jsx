1ef5e577da4005a3ccf434fa78bbdbf46a6ded75
chaemina <chaemina82@naver.com>
2023-11-10 20:33:21 +0900

    fix : refresh token 발급 시 에러 처리
    1. 로딩 처리 시 오류 발생
    2. refreh token 발급 시 에러 -> 새로고침 -> 발급
    원인이 되는 것 같아 수정함

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,39 +1,41 @@
 import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../apis/user";
 import Footer from "./Footer";
 import GNB from "./GNB";
 import { ErrorBoundary } from "react-error-boundary";
 import Loader from "../components/common/Loader";
 import Error from "../components/account/atoms/Error";
-import { UserContent } from "./UserContent";
 
 export default function Layout() {
   const token = window.localStorage.getItem("token");
+  // const { data, isLoading, isError, error } = useQuery(["getUser"], getUser, {
+  //   enabled: !!token,
+  // });
+  // const profileImage = data?.data?.data?.profileImage;
 
-  if (!token) {
-    return (
-      <div className="relative">
-        <GNB profileImage={null} />
-        <main className="pt-20 pb-20 min-h-screen bg-green-100 flex flex-col">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return <Loader />;
+  // }
+
+  // // 401 에러 (토큰 만료) 시 에러 처리 필요
+  // if (isError) {
+  //   return (
+  //     <Error
+  //       error={props.error?.response?.status}
+  //       errorMessage={errorMessage}
+  //     />
+  //   );
+  // }
 
   return (
-    <Suspense fallback={<Loader />}>
-      <ErrorBoundary
-        FallbackComponent={(props) => (
-          <Error
-            error={props.error?.response?.status}
-            errorMessage={props.error.message}
-          />
-        )}
-      >
-        <UserContent enabled={!!token} />
-      </ErrorBoundary>
-    </Suspense>
+    <div className="relative">
+      <GNB profileImage={null} />
+      <main className="pt-20 pb-20 min-h-screen bg-green-100 flex flex-col">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
